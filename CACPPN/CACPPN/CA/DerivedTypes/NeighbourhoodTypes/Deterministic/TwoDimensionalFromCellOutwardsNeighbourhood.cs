4dd0f605d998bb1d6635035a331a10784fdc9c5a
sindr <sindresfjermestad@gmail.com>
2016-09-20 11:16:39 +0200

    made most of the pre-planned types with their inheritance links to the base types

---
+++ CACPPN/CACPPN/CA/DerivedTypes/NeighbourhoodTypes/Deterministic/TwoDimensionalFromCellOutwardsNeighbourhood.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CACPPNConsole.CA.BaseTypes;
+
+namespace ConsoleApplication1.CA.DerivedTypes.NeighbourhoodTypes.Deterministic
+{
+	class TwoDimensionalFromCellOutwardsNeighbourhood : Neighbourhood
+	{
+	}
+}
---
+++ CACPPN/CACPPN/CA/DerivedTypes/StateSetTypes/ContinuousStateSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CACPPNConsole.CA.BaseTypes;
+
+namespace ConsoleApplication1.CA.DerivedTypes.StateSetTypes
+{
+	class ContinuousStateSet : StateSet
+	{
+		//This is a thing that's possible with CPPN representation
+		//Almost worth it's own experiment and question a la:
+		//"where does the granularity 'max out'" (in a minimal sense O:) )
+	}
+}
